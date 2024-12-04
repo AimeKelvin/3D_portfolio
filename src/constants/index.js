7bafad4889a98eb3f90f028fd3db66cc6acbb3d9
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-12-04 19:56:14 +0200

    small regulation in work xp

--- src/constants/index.js
+++ src/constants/index.js
@@ -260,29 +260,13 @@ export const calculateSizes = (isSmall, isMobile, isTablet) => {
 export const workExperiences = [
   {
     id: 1,
-    name: 'Framer',
+    name: 'Chroste Labs',
     pos: 'Lead Web Developer',
-    duration: '2022 - Present',
-    title: "Framer serves as my go-to tool for creating interactive prototypes. I use it to bring designs to  life, allowing stakeholders to experience the user flow and interactions before development.",
-    icon: '/assets/framer.svg',
-    animation: 'victory',
-  },
-  {
-    id: 2,
-    name: 'Figma',
-    pos: 'Web Developer',
-    duration: '2020 - 2022',
-    title: "Figma is my collaborative design platform of choice. I utilize it to work seamlessly with team members and clients, facilitating real-time feedback and design iterations. Its cloud-based.",
-    icon: '/assets/figma.svg',
-    animation: 'clapping',
-  },
-  {
-    id: 3,
-    name: 'Notion',
-    pos: 'Junior Web Developer',
-    duration: '2019 - 2020',
-    title: "Notion helps me keep my projects organized. I use it for project management, task tracking, and as a central hub for documentation, ensuring that everything from design notes to.",
-    icon: '/assets/notion.svg',
+    duration: '2023 - Present',
+    title: "As Lead Developer at Chroste Labs, I oversee the design and development of custom web solutions, ensuring high performance, scalability, and seamless user experiences. I lead a team in building innovative, client-focused websites that drive growth and enhance digital presence.",
+    icon: '/assets/chroste.png',
     animation: 'salute',
   },
+
+  
 ];
